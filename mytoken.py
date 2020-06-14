86312912a4471d82615415c1622f7b89f8251f6b
Muhammad Alif Kurniawan <63390461+muhammadalif-k@users.noreply.github.com>
2020-06-14 07:38:18 +0700

    Create mytoken.py

---
+++ mytoken.py
@@ -0,0 +1,4 @@
+TOKEN ='1119040277:AAH1UFj9ZqgTtZW58v-KZcMU2JqywhRf6S0'
+Reply = "Hallo 👋, Ini Adalah Bot Pertamaku Untuk Tugas PWPB SMK Taruna Bhakti"
+Reply1 = " ~~ Data Siswa Kelas XI Rekayasa Perangkat Lunak ✔ ~~ "
+Help = " ~~ Hallo Apa Yang Bisa saya Bantu? 😊 ~~"
